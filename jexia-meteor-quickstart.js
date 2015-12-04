2be028a6c540662811124596e7fefff535531d5b
Vassilis <vapits@gmail.com>
2015-12-04 11:43:28 +0100

    remove server related code from quickstart file

--- jexia-meteor-quickstart.js
+++ jexia-meteor-quickstart.js
@@ -1,12 +1,3 @@
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-    // Anything you put here is not visible to clients
-    // so you can use it for AUthentication & token matters.
-
-    });
-}
-
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
